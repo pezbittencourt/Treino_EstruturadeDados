dc2be104d3271053643f3abdf3bf1887154480f3
Pedro Zeferino Bittencourt <pedro.zeferinob@gmail.com>
2026-04-28 17:03:56 -0300

    desafio 2 BST

---
+++ Desafios/BST/Desafio2.cs
@@ -0,0 +1,123 @@
+﻿// Nó da árvore (conforme fornecido)
+public class Node
+{
+    public int Key { get; set; }
+    public Node Left { get; set; }
+    public Node Right { get; set; }
+    
+    public Node(int key)
+    {
+        this.Key = key;
+    }
+}
+
+// BST é Binary Search Tree
+public class BST
+{
+ public Node Root { get; set; }
+
+ public class BST()
+ {
+     this.Root() = Null;
+ }
+
+ // Método Público: Inserir
+   public void Insert(int valor){
+        // A raiz da árvore pode mudar se ela estiver vazia, 
+        // por isso reatribuímos a Root com o resultado da recursão.
+        Root = InsertRecursive(Root, valor);
+    }
+
+   private Node InsertRecursive(Node atual, int valor)
+   {
+       //Caso base: No caso de não haver nó algum, cria um nó 
+       if (atual == NULL)
+       {
+           return new Node(valor);
+       }
+
+       if (valor < atual.Key)
+       {
+           atual.Left = InsertRecursive(atual.Left, valor);
+       }
+
+       else if (valor > atual.Key)
+       {
+           atual.Right = InsertRecursive(atual.Right, valor);
+       }
+
+       return atual;
+   }
+
+    //Buscar
+    public Node Search(int value)
+    {
+        return SearchRecursive(Root, value);
+    }
+
+    //Busca Recursiva
+    private Node SearchRecursive(Node atual, int valor)
+    {
+        if (atual == null)
+        {
+            Console.WriteLine("não encontrado");
+            return atual;
+        }
+        elif(atual.Key == valor)
+        {
+            return atual;
+        }
+        if (valor < atual.Key)
+        {
+            return SearchRecursive(atual.Left, valor);
+        }
+
+        return SearchRecursive(atual.Right, valor);
+    }
+
+    public Node BuscarMaximo() 
+    {
+        return MaiorValor(this.root); 
+    }
+    
+    //Maior valor
+    private Node MaiorValor(Node atual)
+    {
+        if (atual == null)
+        {
+            return;
+        }
+        if (atual.Right == null)
+        {
+            return atual;
+        }
+        else
+        {
+            return MaiorValor(atual.Right);
+        }
+    }
+    
+    public Node BuscarMinimo() 
+    {
+        return MenorValor(this.root);
+    }
+    
+    //Menor valor
+    private Node MenorValor(Node atual)
+    {
+        if (atual == null)
+        {
+            return;
+        }
+
+        if (atual.Left == null)
+        {
+            return atual;
+        }
+        else
+        {
+            return MenorValor(atual.Left);
+        }
+    }
+}
+
